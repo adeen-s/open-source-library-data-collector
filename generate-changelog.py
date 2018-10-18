89e049467e34a8e971267c0848a93d2298900f44
Adeen Shukla <adeen@adeen.me>
2018-10-18 22:46:07 +0530

    Fix changelog to be in the correct format

--- generate-changelog.py
+++ generate-changelog.py
@@ -1,7 +1,18 @@
 import requests
 import re
+import datetime
 
-def getChanges():
+def get_last_release():
+    with open("CHANGELOG.md") as file:
+        data = file.read()
+        m = re.search(r"## \[.*\]", data)
+        return ("".join(m.group(0).rsplit(" ", 1)[1:]))[1:-1]
+
+def get_new_release():
+    new_release = input("Last release version was {}, \nEnter the new version number: ".format(get_last_release()))
+    return new_release
+
+def get_changes():
     changes = []
     link = "https://api.github.com/repos/sendgrid/open-source-library-data-collector/"
     releases = requests.get(link + "releases").json()
@@ -16,6 +27,14 @@ def getChanges():
             else:
                 changes.append("{}, #{}".format(pr["title"], pr["number"]))
             changes.append("Big thanks to @{} for the PR!".format(pr["user"]["login"]))
+    changes = "\n- ".join(changes)
     return changes
 
-print(getChanges())
+def generate_changelog():
+    version = get_new_release()
+    changelog = "## [{}] - {}\n".format(version, str(datetime.datetime.today()).split()[0])
+    changelog += "### Fixed"
+    changelog += get_changes()
+    return changelog
+
+print(generate_changelog())
