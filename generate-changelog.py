3f8e0fb0f8e6b4f49656b2865616e9787afc14e8
Adeen Shukla <adeen@adeen.me>
2018-10-18 22:04:55 +0530

    Refactor and modularise the code

--- generate-changelog.py
+++ generate-changelog.py
@@ -1,17 +1,21 @@
 import requests
 import re
-t = {}
-link = "https://api.github.com/repos/sendgrid/open-source-library-data-collector/"
-releases = requests.get(link + "releases").json()
-pulls = requests.get(link + "pulls?state=closed&per_page=100").json()
-for pr in pulls:
-    if(pr["merged_at"] and pr["merged_at"] > releases[0]["created_at"]):
-        s = pr["body"]
-        t = "".join(s.rsplit("-->", 1)[1:])
-        m = re.search(r"[Resolves|Closes|Fixes]+ #(\d+)", t)
-        if m is not None:
-            issue = "".join(m.group(0).rsplit(" ", 1)[1:])
-            print("Issue {} {}, fixed by Pull #{}".format(issue, pr["title"], pr["number"]))
-        else:
-            print("{}, #{}".format(pr["title"], pr["number"]))
-        print("Big thanks to @{} for the PR!".format(pr["user"]["login"]))
+
+def getChanges():
+    changes = []
+    link = "https://api.github.com/repos/sendgrid/open-source-library-data-collector/"
+    releases = requests.get(link + "releases").json()
+    pulls = requests.get(link + "pulls?state=closed&per_page=100").json()
+    for pr in pulls:
+        if(pr["merged_at"] and pr["merged_at"] > releases[0]["created_at"]):
+            message = "".join(pr["body"].rsplit("-->", 1)[1:])
+            m = re.search(r"[Resolves|Closes|Fixes|resolves|closes|fixes]+ #(\d+)", message)
+            if m is not None:
+                issue = "".join(m.group(0).rsplit(" ", 1)[1:])
+                changes.append("Issue {} {}, fixed by Pull #{}".format(issue, pr["title"], pr["number"]))
+            else:
+                changes.append("{}, #{}".format(pr["title"], pr["number"]))
+            changes.append("Big thanks to @{} for the PR!".format(pr["user"]["login"]))
+    return changes
+
+print(getChanges())
